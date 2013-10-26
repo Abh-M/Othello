3988720eba9af47ef04f52fc99f5e963fc37ec1f
AbhineetM <mygitrepo@gmail.com>
2013-10-26 15:07:29 -0400

    Changed eval

--- armajrik.py
+++ armajrik.py
@@ -37,7 +37,7 @@ def evaluation(board):
             else:
                 value = value + 0
 
-    value = value + len(getAllPossibleMovesFromState(deepcopy(board), playerColor)) #- len(getAllPossibleMovesFromState(deepcopy(board), opponentColor)) 
+    value = value + len(getAllPossibleMovesFromState(deepcopy(board), playerColor)) - len(getAllPossibleMovesFromState(deepcopy(board), opponentColor)) 
     return value
 
 
